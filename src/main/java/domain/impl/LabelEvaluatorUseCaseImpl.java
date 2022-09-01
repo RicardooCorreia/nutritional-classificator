e04bb62c2c72ded9889467938c165958d5a67684
Ricardo Correia <ricardo.correia@blip.pt>
2022-09-01 09:10:28 +0100

    Added LabelEvaluatorUseCase

--- src/main/java/domain/impl/LabelEvaluatorUseCaseImpl.java
+++ src/main/java/domain/impl/LabelEvaluatorUseCaseImpl.java
@@ -3,12 +3,14 @@ package domain.impl;
 import domain.api.LabelEvaluatorUseCase;
 import domain.catalog.EvaluationResult;
 import domain.catalog.Label;
+import domain.catalog.LabelLimits;
 import domain.catalog.Measurement;
 import domain.catalog.Nutrient;
 import domain.catalog.NutrientScore;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 public class LabelEvaluatorUseCaseImpl implements LabelEvaluatorUseCase {
@@ -18,11 +20,28 @@ public class LabelEvaluatorUseCaseImpl implements LabelEvaluatorUseCase {
     @Override
     public EvaluationResult evaluate(Label label, Measurement measurement) {
 
-        return new EvaluationResult(Map.of(
-                Nutrient.FAT, NutrientScore.GREEN,
-                Nutrient.SATURATED_FAT, NutrientScore.GREEN,
-                Nutrient.SALT, NutrientScore.GREEN,
-                Nutrient.SUGAR, NutrientScore.GREEN
-        ));
+        return new EvaluationResult(label.getNutrients()
+                .entrySet()
+                .stream()
+                .map(nutrientFloatEntry ->
+                        new NutrientValue(nutrientFloatEntry.getKey(),
+                                nutrientFloatEntry.getValue(),
+                                new LabelLimits.Key(nutrientFloatEntry.getKey(), measurement)))
+                .map(nutrientValue -> calculateScore(nutrientValue.nutrient(), nutrientValue.value(), configuration.getLabelLimit(nutrientValue.labelLimitsKey())))
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+    }
+
+    private Map.Entry<Nutrient, NutrientScore> calculateScore(Nutrient nutrient, float value, LabelLimits.Limits labelLimit) {
+
+        if (value < labelLimit.lower()) {
+            return Map.entry(nutrient, NutrientScore.GREEN);
+        } else if (value > labelLimit.upper()) {
+            return Map.entry(nutrient, NutrientScore.RED);
+        } else {
+            return Map.entry(nutrient, NutrientScore.YELLOW);
+        }
+    }
+
+    private record NutrientValue(Nutrient nutrient, float value, LabelLimits.Key labelLimitsKey) {
     }
 }
--- src/test/java/domain/impl/LabelEvaluatorUseCaseImplTest.java
+++ src/test/java/domain/impl/LabelEvaluatorUseCaseImplTest.java
@@ -39,10 +39,10 @@ class LabelEvaluatorUseCaseImplTest {
     void evaluate_whenAllValuesAreGoodValue_returnGoodResult() {
 
         // Given
-        final float fat = faker.number().randomNumber();
-        final float saturatedFat = faker.number().randomNumber();
-        final float sugar = faker.number().randomNumber();
-        final float salt = faker.number().randomNumber();
+        final float fat = faker.number().randomDigitNotZero();
+        final float saturatedFat = faker.number().randomDigitNotZero();
+        final float sugar = faker.number().randomDigitNotZero();
+        final float salt = faker.number().randomDigitNotZero();
         final Label label = new Label(fat, saturatedFat, sugar, salt);
         final Measurement measurement = Measurement.GRAM;
 
@@ -82,8 +82,159 @@ class LabelEvaluatorUseCaseImplTest {
                         ));
     }
 
-    private LabelLimits.Limits underLowerLimit(float fat) {
+    @Test
+    void evaluate_whenAllValuesAreMediumValue_returnYellowResult() {
 
-        return new LabelLimits.Limits(fat + 0.5f, fat + 0.3f);
+        // Given
+        final float fat = faker.number().randomDigitNotZero();
+        final float saturatedFat = faker.number().randomDigitNotZero();
+        final float sugar = faker.number().randomDigitNotZero();
+        final float salt = faker.number().randomDigitNotZero();
+        final Label label = new Label(fat, saturatedFat, sugar, salt);
+        final Measurement measurement = Measurement.MILLILITER;
+
+
+        final LabelLimits.Limits fatLimits = overLowerLimitUnderUpperLimit(fat);
+        doReturn(fatLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.FAT, measurement)));
+
+        final LabelLimits.Limits saturatedFatLimits = overLowerLimitUnderUpperLimit(saturatedFat);
+        doReturn(saturatedFatLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.SATURATED_FAT, measurement)));
+
+        final LabelLimits.Limits sugarLimits = overLowerLimitUnderUpperLimit(sugar);
+        doReturn(sugarLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.SUGAR, measurement)));
+
+        final LabelLimits.Limits saltLimits = overLowerLimitUnderUpperLimit(salt);
+        doReturn(saltLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.SALT, measurement)));
+
+        // When
+        final EvaluationResult result = subject.evaluate(label, measurement);
+
+        // Then
+        assertThat(result).isNotNull();
+        assertThat(result.nutrientScores())
+                .containsExactlyInAnyOrderEntriesOf(
+                        Map.of(
+                                Nutrient.FAT, NutrientScore.YELLOW,
+                                Nutrient.SATURATED_FAT, NutrientScore.YELLOW,
+                                Nutrient.SUGAR, NutrientScore.YELLOW,
+                                Nutrient.SALT, NutrientScore.YELLOW
+                        ));
+    }
+
+    @Test
+    void evaluate_whenAllValuesAreBadValue_returnRedResult() {
+
+        // Given
+        final float fat = faker.number().randomDigitNotZero();
+        final float saturatedFat = faker.number().randomDigitNotZero();
+        final float sugar = faker.number().randomDigitNotZero();
+        final float salt = faker.number().randomDigitNotZero();
+        final Label label = new Label(fat, saturatedFat, sugar, salt);
+        final Measurement measurement = Measurement.MILLILITER;
+
+
+        final LabelLimits.Limits fatLimits = overUpperLimit(fat);
+        doReturn(fatLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.FAT, measurement)));
+
+        final LabelLimits.Limits saturatedFatLimits = overUpperLimit(saturatedFat);
+        doReturn(saturatedFatLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.SATURATED_FAT, measurement)));
+
+        final LabelLimits.Limits sugarLimits = overUpperLimit(sugar);
+        doReturn(sugarLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.SUGAR, measurement)));
+
+        final LabelLimits.Limits saltLimits = overUpperLimit(salt);
+        doReturn(saltLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.SALT, measurement)));
+
+        // When
+        final EvaluationResult result = subject.evaluate(label, measurement);
+
+        // Then
+        assertThat(result).isNotNull();
+        assertThat(result.nutrientScores())
+                .containsExactlyInAnyOrderEntriesOf(
+                        Map.of(
+                                Nutrient.FAT, NutrientScore.RED,
+                                Nutrient.SATURATED_FAT, NutrientScore.RED,
+                                Nutrient.SUGAR, NutrientScore.RED,
+                                Nutrient.SALT, NutrientScore.RED
+                        ));
+    }
+
+    @Test
+    void evaluate_whenValuesAreMixedValue_returnMixedResult() {
+
+        // Given
+        final float fat = faker.number().randomDigitNotZero();
+        final float saturatedFat = faker.number().randomDigitNotZero();
+        final float sugar = faker.number().randomDigitNotZero();
+        final float salt = faker.number().randomDigitNotZero();
+        final Label label = new Label(fat, saturatedFat, sugar, salt);
+        final Measurement measurement = Measurement.MILLILITER;
+
+
+        final LabelLimits.Limits fatLimits = overUpperLimit(fat);
+        doReturn(fatLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.FAT, measurement)));
+
+        final LabelLimits.Limits saturatedFatLimits = underLowerLimit(saturatedFat);
+        doReturn(saturatedFatLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.SATURATED_FAT, measurement)));
+
+        final LabelLimits.Limits sugarLimits = overLowerLimitUnderUpperLimit(sugar);
+        doReturn(sugarLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.SUGAR, measurement)));
+
+        final LabelLimits.Limits saltLimits = overLowerLimitUnderUpperLimit(salt);
+        doReturn(saltLimits)
+                .when(configuration)
+                .getLabelLimit(eq(new LabelLimits.Key(Nutrient.SALT, measurement)));
+
+        // When
+        final EvaluationResult result = subject.evaluate(label, measurement);
+
+        // Then
+        assertThat(result).isNotNull();
+        assertThat(result.nutrientScores())
+                .containsExactlyInAnyOrderEntriesOf(
+                        Map.of(
+                                Nutrient.FAT, NutrientScore.RED,
+                                Nutrient.SATURATED_FAT, NutrientScore.GREEN,
+                                Nutrient.SUGAR, NutrientScore.YELLOW,
+                                Nutrient.SALT, NutrientScore.YELLOW
+                        ));
+    }
+
+    private LabelLimits.Limits underLowerLimit(float value) {
+
+        return new LabelLimits.Limits(value + 0.5f, value + 0.3f);
+    }
+
+    private LabelLimits.Limits overLowerLimitUnderUpperLimit(float value) {
+
+        return new LabelLimits.Limits(value + 0.5f, value - 0.3f);
+    }
+
+    private LabelLimits.Limits overUpperLimit(float value) {
+
+        return new LabelLimits.Limits(value - 0.3f, value - 0.5f);
     }
 }
